f3348c24cfbd4c051303b5446e96d2e3009a27d5
Carlos Galdamez <cgaldamez14@gmail.com>
2016-04-18 22:10:19 -0700

    test clients on local host

---
+++ src/testUtillities/Test.java
@@ -0,0 +1,12 @@
+package testUtillities;
+import java.io.IOException;
+import java.net.UnknownHostException;
+
+import communication.RoverClient;
+
+public class Test {
+
+	public static void main(String[] args) throws UnknownHostException, IOException {
+		new RoverClient("127.0.0.1", 8000,"ROVER_100");
+		}
+}
