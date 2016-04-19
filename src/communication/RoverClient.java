e04a4d330f08d9af1b771ca5c6d711f6b795be2a
Carlos Galdamez <cgaldamez14@gmail.com>
2016-04-18 17:10:36 -0700

    changed implementation of both classes

--- src/communication/RoverClient.java
+++ src/communication/RoverClient.java
@@ -6,46 +6,50 @@ import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.InetAddress;
 import java.net.Socket;
-import java.util.Scanner;
 
 
 // NOTE: Might need to send request back to client...
-public class RoverClient implements Runnable{
+public class RoverClient{
 	private String roverName;
-	private Scanner in;
+	//private Scanner in;
 	private Socket socket;
 	private static PrintWriter output;
 	private BufferedReader input;
 
-	
+
 	public RoverClient(String ip, int port, String name) throws IOException{
 		this.socket = new Socket(ip, port);
 		roverName = name;
-		in = new Scanner(System.in); // Most likely wont need this but just going to use scanner for testing purposes
+		//in = new Scanner(System.in); // Most likely wont need this but just going to use scanner for testing purposes
 		input = new BufferedReader(new InputStreamReader(this.socket.getInputStream()));
 		output = new PrintWriter(this.socket.getOutputStream(), true);
 	} 
-	
+
 	public RoverClient(Socket socket, String name) throws IOException{
 		this.socket = socket;
 		roverName = name;
-		in = new Scanner(System.in); // Most likely wont need this but just going to use scanner for testing purposes
+		//in = new Scanner(System.in); // Most likely wont need this but just going to use scanner for testing purposes
 		input = new BufferedReader(new InputStreamReader(this.socket.getInputStream()));
 		output = new PrintWriter(this.socket.getOutputStream(), true);
+
+		Thread read = new Thread(){
+			public void run() {
+				while(true){
+					try {
+						if(input.ready())
+							System.out.println(input.readLine());
+					} catch (IOException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+
+				}
+			}
+		};
+		
+		read.start();
 	} 
 
-	@Override
-	public void run() {
-		while(true){
-			try {
-				this.recv();
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-
-		}
-	}
 
 	public String getRoverName() {
 		return roverName;
@@ -56,21 +60,16 @@ public class RoverClient implements Runnable{
 		output.println(message);
 	}
 
-	// This will not be used either only for testing purposes
-	public void send(){
-		send(roverName + ": " + in.nextLine());
-	}
 
+//	public void recv() throws IOException{
+//		if(input.ready())
+//			System.out.println(input.readLine());
+//	}
 
-	public void recv() throws IOException{
-		if(input.ready())
-			System.out.println(input.readLine());
-	}
-	
 	public InetAddress getIP(){
 		return socket.getInetAddress();
 	}
-	
+
 	public int getPort(){
 		return socket.getPort();
 	}
--- src/communication/RoverServer.java
+++ src/communication/RoverServer.java
@@ -7,30 +7,33 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class RoverServer implements Runnable{
-	
+
 	// Rover name and listerning port
 	private final static String NAME = "ROVER_03";
 	private final static int PORT = 8000;
-	
+
 	// List of connected rovers
 	private List<RoverClient> rovers;
-	
-	
+
+
 	private ServerSocket serverSocket;
-	
+
 	// Will be used to get input from console
 	private InputStreamReader cin;
 	private StringBuilder message;
-	
+
 	public RoverServer() throws IOException{
 		// Creates a server socket at specified port
 		serverSocket = new ServerSocket(PORT);
-		
+
 		System.out.println("ROVER_03 server online...");
 		System.out.println("Waiting for other rovers to connect...");
-		
+
 		cin = new InputStreamReader(System.in);
 		rovers = new ArrayList<>();
+		
+		// Begin messaging thread
+		sendMessage();
 	}
 
 	@Override
@@ -38,17 +41,37 @@ public class RoverServer implements Runnable{
 		while(true){
 			try {
 				RoverClient client = new RoverClient(serverSocket.accept(), NAME);
-				Thread newThread = new Thread(client);
 				rovers.add(client);
-				newThread.start();
 				getClientList();
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
 		}
 	}
-	
-	public  void getClientList(){
+
+	public void sendMessage(){
+		Thread messages = new Thread(){
+			public void run(){
+				while(true){
+					if(!rovers.isEmpty()){
+						try {
+							while(cin.ready()){
+								message.append((char)cin.read());
+							}
+						} catch (IOException e) {
+							e.printStackTrace();
+						}
+						for(RoverClient r : rovers){
+							r.send(message.toString());
+						}
+					}
+				}
+			}
+		};
+		messages.start();
+	}
+
+	public void getClientList(){
 		System.out.println("****************** Rover List **********************");
 		for(RoverClient r : rovers){
 			System.out.println("Address: " + r.getIP() + " , Port:" + r.getPort());
