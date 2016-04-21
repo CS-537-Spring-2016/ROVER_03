b21a3332f5849d964fbfe811a6e3c33fea357bf7
Carlos Galdamez <cgaldamez14@gmail.com>
2016-04-20 19:19:25 -0700

    added comments of functions that need to be made , refactored code to make it more understandable

--- src/communication/RoverClient.java
+++ src/communication/RoverClient.java
@@ -7,88 +7,80 @@ import java.io.PrintWriter;
 import java.net.InetAddress;
 import java.net.Socket;
 
+/* Need to implement method that filters rovers by tool that is needed either to harvets or drill
+ * and another method that searches from that filtered list the closest rover to my location */
 
-// NOTE: Might need to send request back to client...
-public class RoverClient{
+public class RoverClient implements Runnable{
 	private String roverName;
-	//private Scanner in;
 	private Socket socket;
 	private PrintWriter output;
 	private BufferedReader input;
 
-
+	/* This constructor will be used when you want to send a request to establish a connection,
+	 * unlike the other constructor in this class you create the socket in the constructor
+	 * to try to extablish a connection with another rover using the destination IP and 
+	 * destination port.
+	 * */
 	public RoverClient(String ip, int port, String name) throws IOException{
+
 		this.socket = new Socket(ip, port);
 		roverName = name;
-		//in = new Scanner(System.in); // Most likely wont need this but just going to use scanner for testing purposes
-		input = new BufferedReader(new InputStreamReader(this.socket.getInputStream()));
-		output = new PrintWriter(this.socket.getOutputStream(), true);
-		
-		Thread read = new Thread(){
-			public void run() {
-				while(true){
-					try {
-						if(input.ready())
-							System.out.println(input.readLine());
-					} catch (IOException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					}
-
-				}
-			}
-		};
-		
-		read.start();
 	} 
 
+	/* This constructor will be used when someone sent a request to establish a connection,
+	 * the socket argment it takes is the socket that is created by serverSocket.accept()
+	 * which extablishes the connection. So instead of trying to establish a connection in
+	 * this constructor you are just storing a reference to the socket that was created
+	 * by using serverSocket.accept() in the RoverServer class.
+	 * */
 	public RoverClient(Socket socket, String name) throws IOException{
+
 		this.socket = socket;
 		roverName = name;
-		//in = new Scanner(System.in); // Most likely wont need this but just going to use scanner for testing purposes
-		input = new BufferedReader(new InputStreamReader(this.socket.getInputStream()));
-		output = new PrintWriter(this.socket.getOutputStream(), true);
 
-		Thread read = new Thread(){
-			public void run() {
-				while(true){
-					try {
-						if(input.ready())
-							System.out.println(input.readLine());
-					} catch (IOException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					}
-
-				}
-			}
-		};
-		
-		read.start();
 	} 
-
-
+	
+	// Return rover name
 	public String getRoverName() {
 		return roverName;
 	}
 
-	// Will be used to send messages to other rovers
+	// Send message to other rover
 	public void send(String message){
 		output.println(message);
 	}
 
-
-//	public void recv() throws IOException{
-//		if(input.ready())
-//			System.out.println(input.readLine());
-//	}
-
+	// return IP address for other rover in the form /xxx.xx.xx.x
 	public InetAddress getIP(){
 		return socket.getInetAddress();
 	}
-
+	
+	// returns port number of other rover
 	public int getPort(){
 		return socket.getPort();
+	}
+
+	@Override
+	public void run() {	// Thread that will listen for incoming messages
+		try {
+			// Instatiates buffered reader and print writer which will be used to send and receive messages through this socket
+			input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+			output = new PrintWriter(socket.getOutputStream(), true);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		// Sends message to other rover so they know a connection has been extablished
+		send("You are now connected to " + roverName);
+
+		while(true){
+			try {
+				if(input.ready())
+					System.out.println(input.readLine());
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}		
 	}
 }
 
--- src/communication/RoverServer.java
+++ src/communication/RoverServer.java
@@ -6,15 +6,19 @@ import java.net.ServerSocket;
 import java.util.ArrayList;
 import java.util.List;
 
+
+/* NOTE TO SELF: Still need to implement method to reconnected to a rover if connection is lost.*/
 public class RoverServer implements Runnable{
 
-	// Rover name and listerning port
+	/* Rover name and listerning port
+	 * If this code is shared with a rovers in blue corporation these two constants have to be changed
+	 * These only apply to ROVER_03
+	 */
 	private final static String NAME = "ROVER_03";
 	private final static int PORT = 8000;
 
 	// List of connected rovers
 	private List<RoverClient> rovers;
-
 
 	private ServerSocket serverSocket;
 
@@ -23,13 +27,19 @@ public class RoverServer implements Runnable{
 	private StringBuilder message = new StringBuilder();
 
 	public RoverServer() throws IOException{
-		// Creates a server socket at specified port
+		// Creates a server socket at specified port and binds it to a specified port and address of local machine
 		serverSocket = new ServerSocket(PORT);
 
-		System.out.println("ROVER_03 server online...");
+		System.out.println(NAME + " server online...");
 		System.out.println("Waiting for other rovers to connect...");
 
+		/* instatiates input stream reader object that will be used to capture any input written on the console.
+		 * since the rovers are autonomous we will not be needing this for final implementation because there will be no
+		 * human interaction with the rover but it is needed for testing purposes
+		 * */
 		cin = new InputStreamReader(System.in);
+		
+		// Array list that keeps track of connected rovers
 		rovers = new ArrayList<>();
 
 		// Begin messaging thread
@@ -37,42 +47,35 @@ public class RoverServer implements Runnable{
 	}
 
 	@Override
-	public void run() {
+	public void run() {	// Thread that continously listens for incoming connections
 		while(true){
 			try {
 				RoverClient client = new RoverClient(serverSocket.accept(), NAME);
 				rovers.add(client);
-				getClientList();
+				new Thread(client).start(); // instantiates and starts a new thread for a connecting client
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
 		}
 	}
 
+	/* This method sends messages that are written on the console to all rovers connected
+	 * how ever again this thread will not be needed for the final implementation and is only for 
+	 * testing purposes */
 	public void sendMessage(){
 		Thread messages = new Thread(){
 			public void run(){
 				while(true){
-					//					try {
-					//						if(cin.ready()){
-					//							System.out.println((char)cin.read());
-					//						}
-					//					} catch (IOException e1) {
-					//						// TODO Auto-generated catch block
-					//						e1.printStackTrace();
-					//					}
-
 					try {
-						//						System.out.println(cin.ready());
-						//						Thread.sleep(5000);
-						if(cin.ready())
+						if(cin.ready())					// Have to put it in a separate if statement because otherwise it does not work
+														// not sure why though
 							if(!rovers.isEmpty()){
-
 								while(cin.ready()){
-									message.append((char)cin.read());
+									message.append((char)cin.read());	// appends character by character to string builder
 									System.out.println(cin.ready());
 								}
-
+								
+								// iterates through connected client array list and calls the send message function
 								for(RoverClient r : rovers){
 									System.out.println(message.toString());
 									System.out.println("sending to " + r.getPort() + " message: " + message );
@@ -81,7 +84,6 @@ public class RoverServer implements Runnable{
 								message.setLength(0); // Reset StringBuilder
 							}
 					} catch (IOException e) {
-						// TODO Auto-generated catch block
 						e.printStackTrace();
 					}
 				}
@@ -89,16 +91,44 @@ public class RoverServer implements Runnable{
 		};
 		messages.start();
 	}
+	
+	/* Rovers receive LOC from server in the following form: "LOC xpos ypos"
+	 * If a rover is trying to tell another rover to go get a piece of science at
+	 * another location the exact string should be sent to the other rover. The 
+	 * receiver of the coordinates will be responsible for extracting the coordinates
+	 * not the sender. Therefore argument taken by this method is the exact same string
+	 * given by the server when the LOC command is sent along with the index in the 
+	 * arraylist of the rover that it is being sent to. Example: LOC 35 65 1 where
+	 * LOC 35 65 is the string from the server and 1 is the index of the rover in 
+	 * your array list.
+	 * */
+	public void sendLOC(String location){
+		int index = getIndex(location);
+		rovers.get(index).send(getLocation(location));
+	}
+	
+	private int getIndex(String location){
+		/* Splits the location string which has the form "LOC 35 65 1" into an array of
+		 * four components the last one at index 3 is the index of the rover client in the
+		 * array. Trim is used because to make sure you dont get a NumberFormatException when trying
+		 * to parse the string into and integer.
+		 */
+		return Integer.parseInt(location.split(" ")[3].trim());
+	}
+	
+	/* Extracts only the location portion of the message including the LOC so receiver rover can use the 
+	 * extractLOC() method given provided Richard 
+	 */
+	private String getLocation(String location){
+		String[] split = location.split(" "); 
+		return split[0] + " " + split[1] + " " + split[2];
+	}
 
+	// prints list to console of connected rovers
 	public void getClientList(){
 		System.out.println("****************** Rover List **********************");
 		for(RoverClient r : rovers){
 			System.out.println("Address: " + r.getIP() + " , Port:" + r.getPort());
 		}
-	}
-
-	public static void main(String args[]) throws IOException{
-		Thread newThread = new Thread(new RoverServer());
-		newThread.start();
 	}
 }
