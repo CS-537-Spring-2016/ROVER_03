803c71f9f9b0286b21a2199bf763ae32c186af9e
Carlos Galdamez <cgaldamez14@gmail.com>
2016-04-25 16:06:47 -0700

    added methods that will assist it modifying contents on queue

--- src/model/RoverQueue.java
+++ src/model/RoverQueue.java
@@ -7,7 +7,7 @@ public class RoverQueue {
 	List<String> locationList = new ArrayList<String>();
 	
 	public RoverQueue(){
-		
+		locationList.add("LOC 45 20");
 	}
 	
 	public void addLocation (String location){
@@ -19,6 +19,18 @@ public class RoverQueue {
 			System.out.println(str);
 		}
 		
+	}
+	
+	public boolean isEmpty(){
+		return locationList.isEmpty();
+	}
+	
+	public void removeCompletedJob(){
+		locationList.remove(0);
+	}
+	
+	public String getJob(){
+		return locationList.get(0);
 	}
 	
 }
