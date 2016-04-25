c8ea9d6d6856e4c7827e2698b12f88c5451f20da
Carlos Galdamez <cgaldamez14@gmail.com>
2016-04-25 16:08:16 -0700

    created tracking class that will keep track of rovers position relative to its destination

---
+++ src/trackingUtility/Tracker.java
@@ -0,0 +1,72 @@
+package trackingUtility;
+
+import common.Coord;
+
+public class Tracker {
+
+	private Coord returnLocation;
+	private Coord targetLocation;
+
+	private Coord startingPoint;
+	private Coord destination;
+
+	// Keeps track of how may tiles are left to go
+	private Coord distanceTracker;
+	
+	public Tracker(){
+		distanceTracker = new Coord(0,0);
+	}
+
+	public Coord getReturnLocation() {
+		return returnLocation;
+	}
+
+	public void setReturnLocation(Coord returnLocation) {
+		this.returnLocation = returnLocation;
+	}
+
+	public Coord getTargetLocation() {
+		return targetLocation;
+	}
+
+	public void setTargetLocation(Coord targetLocation) {
+		this.targetLocation = targetLocation;
+	}
+
+	public Coord getStartingPoint() {
+		return startingPoint;
+	}
+
+	public void setStartingPoint(Coord startingPoint) {
+		this.startingPoint = startingPoint;
+	}
+
+	public Coord getDestination() {
+		return destination;
+	}
+
+	public void setDestination(Coord destination) {
+		this.destination = destination;
+	}
+
+	public Coord getDistanceTracker() {
+		return distanceTracker;
+	}
+
+	public void setDistanceTracker() {
+		distanceTracker.xpos = destination.xpos - startingPoint.xpos;
+		distanceTracker.ypos = destination.ypos - startingPoint.ypos;
+	}
+
+	public void updateXPos(int x){
+		distanceTracker.xpos += x; 
+	}
+	
+	public void updateYPos(int y){
+		distanceTracker.xpos += y; 
+	}
+	
+	public boolean hasArrived(){
+		return distanceTracker.xpos == 0 && distanceTracker.ypos == 0;
+	}
+}
