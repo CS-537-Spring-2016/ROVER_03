c30819dbc63665f1d2e22a7ffc32542530f24376
BTARPAR <tarpara_bhargav@yahoo.in>
2016-04-28 15:52:00 -0700

    Problem With Sand

--- src/swarmBots/ROVER_00.java
+++ src/swarmBots/ROVER_00.java
@@ -201,8 +201,7 @@ public class ROVER_00 {
 					// tile S = y + 1; N = y - 1; E = x + 1; W = x - 1
 	
 					if (goingSouth) {
-						// check scanMap to see if path is blocked to the south
-						// (scanMap may be old data by now)
+						
 						if (scanMapTiles[centerIndex][centerIndex +1].getHasRover() 
 								|| scanMapTiles[centerIndex][centerIndex +1].getTerrain() == Terrain.ROCK || scanMapTiles[centerIndex][centerIndex -1].getTerrain() == Terrain.SAND
 								|| scanMapTiles[centerIndex][centerIndex +1].getTerrain() == Terrain.NONE) {
@@ -214,10 +213,6 @@ public class ROVER_00 {
 						}
 						
 					} else {
-						// check scanMap to see if path is blocked to the north
-						// (scanMap may be old data by now)
-						//System.out.println("ROVER_00 scanMapTiles[2][1].getHasRover() " + scanMapTiles[2][1].getHasRover());
-						//System.out.println("ROVER_00 scanMapTiles[2][1].getTerrain() " + scanMapTiles[2][1].getTerrain().toString());
 						
 						if (scanMapTiles[centerIndex][centerIndex -1].getHasRover() 
 								|| scanMapTiles[centerIndex][centerIndex -1].getTerrain() == Terrain.ROCK || scanMapTiles[centerIndex][centerIndex -1].getTerrain() == Terrain.SAND
