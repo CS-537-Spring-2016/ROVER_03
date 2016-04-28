2eaae5a9f44fa6effe37a9d5ad49445cf4efb6c4
BTARPAR <tarpara_bhargav@yahoo.in>
2016-04-28 15:49:59 -0700

    Stuck In Sand

--- src/swarmBots/ROVER_00.java
+++ src/swarmBots/ROVER_00.java
@@ -204,7 +204,7 @@ public class ROVER_00 {
 						// check scanMap to see if path is blocked to the south
 						// (scanMap may be old data by now)
 						if (scanMapTiles[centerIndex][centerIndex +1].getHasRover() 
-								
+								|| scanMapTiles[centerIndex][centerIndex +1].getTerrain() == Terrain.ROCK || scanMapTiles[centerIndex][centerIndex -1].getTerrain() == Terrain.SAND
 								|| scanMapTiles[centerIndex][centerIndex +1].getTerrain() == Terrain.NONE) {
 							blocked = true;
 						} else {
